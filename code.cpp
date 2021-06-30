a9f37d4c91d2e5a6205f922471dc9b40e3454a83
Aman Tiwari <75423107+aman-tiwari-05@users.noreply.github.com>
2021-06-30 14:08:14 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,61 +4,116 @@ using namespace std;
 
 
 class User{
-    private:
-        string name,roll_no;
+   private:
+        string name , password;
 
 
     public:
         static int score;
-        string subject;
-        void get_details(){
+        string subject,roll_no;
+        
+        void sign_up(){
+            cout<<"----------SIGN UP-----------\n";
             cout<<"Enter your name: ";
+            cin.ignore();
             getline(cin,name);
 
-            cout<<"Roll no-";
+            cout<<"Roll no- ";
             cin>>roll_no;
-            cout<<"Enter Subject of which you wanna take test: ";
-            cin>>subject;
-
-
+            cout<<"Enter password- ";
+            cin>>password;
+            string FileName=roll_no+".txt";
+            fstream NewFile(FileName,ios::in | ios::out | ios::app);
+            NewFile<<roll_no<<" "<<password<<" "<<name<<endl;
+            NewFile.close();
         }
+        
+        void login(){
+            cout<<"---------LOGIN----------\n";
+            string rollno,pass,r,p;
+            A: cout<<"Enter your roll no. : ";
+            cin>>rollno;
+            roll_no=rollno;
+            cout<<"Enter you password: ";
+            cin>>pass;
+            string filename=rollno+".txt";
+            fstream File(filename,ios::in | ios::out | ios::app);
+            File>>r>>p;
+            
+            if (rollno==r && pass==p){
+                cout<<"Login Successfull !!\n";
+            }
+            else{
+                cout<<"Your roll no or password does'nt match , Try again!! \n";
+                File.close();
+                goto A;
+            }
+            File.close();
+            
+            
+            
+            
+        }
+        
         void score_inc(int m){
 
             score+=m;
         }
-        void show_result(){
-            cout<<"Name- "<<name;
-            cout<<"Roll No.- "<<roll_no;
-            cout<<"Your Score- "<<score;
-        }
+        
 };
+
+int User:: score=0;
 
 void get_option(int n,User u1,string topic);
 void ask_ques(int n,string topic);
 
 //Class for subject 1
-class Subject1{
+class Science{
     private:
         string topic;
     public:
         void get_topic(){
-            cout<<"Enter topic: ";
+            cout<<"Enter topic from : 'Physics' or 'Chemistry' ";
+            
             cin>>topic;
-            topic=topic;
+            
+            
         }
         void get_ques(User u1){
             string quesFile=topic+".txt";
             string ansFile=topic+"_ans.txt";
-            int arr[3]={3,1,2};
-            for(int i=0;i<3;i++){
+            int arr[5]={3,1,2,5,4};
+            for(int i=0;i<5;i++){
 
                 ask_ques(arr[i],quesFile);
                 get_option(arr[i],u1,ansFile);
 
         }
         }
+        void add_result(User u1){
+            string filename=u1.roll_no+".txt";
+            fstream FileIn(filename,ios::in | ios::out | ios::app);
+            
+            FileIn<<"Topic- "<<topic<<" Your Score- "<<u1.score<<endl;
+            FileIn.close();
+            
+            
+            
+        }
 };
-int User:: score=0;
+//To show user's file
+void show_file(User u1){
+    string filename=u1.roll_no+".txt";
+    fstream FileOut(filename,ios::in | ios::out | ios::app);
+    string ou;
+    cout<<endl;
+    while(getline(FileOut,ou)){
+    cout<<ou<<"\n";
+
+    }
+    FileOut.close();
+}
+
 
 //Input user choice
 void get_ans(string anss,User u1){
@@ -112,17 +167,49 @@ void ask_ques(int n,string topic){
 // MAIN FUNCTION
 int main()
 {  User u1;
-   u1.get_details();
+    int n;
+    cout<<"Enter '1' if you are existing user\n"<<"'2' for New user \n";
+    
+    cin>>n;
+    if(n==1){
+        u1.login();
+    }
+    else{
+        u1.sign_up();
+        u1.login();
+    }
+    
    cout<<u1.subject<<endl;
-   if (u1.subject=="subject1"){
-       Subject1 t;
-       t.get_topic();
-       t.get_ques(u1);
+   int m;
+   B:cout<<"\nPress '1' to get your marksheet , '2' to take test";
+   cin>>m;
+   switch(m){
+       case 1:
+            show_file(u1);
+            break;
+       case 2:
+            string sub;
+            cout<<"Enter Subject- ";
+            cin>>sub;
+            u1.subject=sub;
+            if (u1.subject=="Science"){
+               Science t;
+               t.get_topic();
+               t.get_ques(u1);
+               t.add_result(u1);
+               goto B;
+               break;
+       
    }
+            
+   }
+    
+   
+   
 
 
 
-   u1.show_result();
+   
    string endd;
    getline(cin,endd);
 
